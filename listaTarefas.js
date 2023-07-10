640b958d490935bb5b509e9888805dee3f8f11c4
SrVertex <121757774+SrVertex@users.noreply.github.com>
2023-07-10 14:08:36 -0300

    3

--- listaTarefas.js
+++ listaTarefas.js
@@ -1,8 +1,30 @@
 const criarTarefa = (evento) => {
     evento.preventDefault();
+
+
+    const lista = document.querySelector("[data-list]");
+
+
     const input = document.querySelector('[data-input-form]');
     const valor = input.value;
-    console.log ( valor );
+
+
+    const tarefa = document.createElement("li");
+    tarefa.classList.add("task")
+
+    const conteudo = `<p class="content">${valor}</p>`;
+
+
+
+
+
+    tarefa.innerHTML = conteudo;
+
+    lista.appendChild(tarefa);
+
+
+
+
     input.value = " ";
 
 }
@@ -10,3 +32,11 @@ const criarTarefa = (evento) => {
 const novaTarefa = document.querySelector("[data-form-button]");
 novaTarefa.addEventListener('click', criarTarefa);
 
+const botaoConclui = () => {
+    const botaoConclui = document.createElement("button");
+
+    botaoConclui.addEventListener("click");
+
+    return botaoConclui;
+
+}
