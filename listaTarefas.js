dd4a3111e472b60ab118cd414cc1cfde9d79c379
SrVertex <121757774+SrVertex@users.noreply.github.com>
2023-07-10 10:56:00 -0300

    3

--- listaTarefas.js
+++ listaTarefas.js
@@ -1,7 +1,12 @@
-const criarTarefa = () => {
-    
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-input-form]');
+    const valor = input.value;
+    console.log ( valor );
+    input.value = " ";
+
 }
 
 const novaTarefa = document.querySelector("[data-form-button]");
-
+novaTarefa.addEventListener('click', criarTarefa);
 
